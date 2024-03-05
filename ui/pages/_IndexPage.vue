206f80f143996c3f014e81b344afaa89baf523f8
Chris Calo <chris.calo@gmail.com>
2024-03-05 18:40:05 -0500

    update _IndexPage.vue to use the composition API
    This addresses the entire reason for creating this repo.
    
    More details:
    https://github.com/withastro/astro/issues/10333

--- ui/pages/_IndexPage.vue
+++ ui/pages/_IndexPage.vue
@@ -1,12 +1,7 @@
-<script>
+<script setup>
   import { ref } from "vue";
+  // the ! prefix is an alias for the ui folder
   import Counter from "!/components/Counter.vue";
-  
-  export default {
-    components: {
-      Counter,
-    },
-  };
 </script>
 
 <template>
